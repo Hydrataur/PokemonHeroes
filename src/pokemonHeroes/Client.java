f1ad2d04af22b054e9e9f89568a44085ff8e2b0f
Hydrataur <rexzhydrataur@gmail.com>
2019-05-22 16:18:54 +0300

    Added comments

--- src/pokemonHeroes/Client.java
+++ src/pokemonHeroes/Client.java
@@ -29,18 +29,19 @@ public class Client {
     }
 
 
+    //Send a message to the other player client
     public void send(int id, int x, int y, boolean quit, double damage) {
-        String line;
-        if (quit)
+        String line; //Create the string we plan to send
+        if (quit) //If we want to send a quit message then the string must be Bye so that the other clients and the server know to shut down too
             line = "Bye";
-        else
+        else //If we're not quitting then we want to send the id of the sender and the coordinates of the click
             line = id + "&&" + x + "&&" + y;
-        if (damage != 0)
+        if (damage != 0) //We wanna make sure the damage is the same for both players since it's randomized. If damage is 0 then no attack was commenced
             line += "&&" + damage;
-        System.out.println("ding");
-        try {
-            out.write(line + "\n");
-            out.flush();
+        System.out.println("ding"); //Debugging
+        try { //We use try to make sure there's no crash in case of an error when sending the message
+            out.write(line + "\n"); //Add the string to the stream
+            out.flush(); //Sends the prepared message
 
         } catch (IOException e) {
             System.err.println("Couldn't read or write");
@@ -48,15 +49,16 @@ public class Client {
         }
     }
 
+    //Send a message to the Android client
     public void sendAndroid(String name, int hp, int maxhp, int attack
 
             , int defense, int mindamage, int maxdamage, int movement, boolean fly, boolean ranged){
-        String line;
+        String line; //Create the string we plan to send
         line = "Android&&" + name.toLowerCase() + "&&" + hp + "&&" + maxhp + "&&" + attack + "&&" + defense
-                + "&&" + mindamage + "&&" + maxdamage + "&&" + movement + "&&" + fly + "&&" + ranged;
-        try {
-            out.write(line + "\n");
-            out.flush();
+                + "&&" + mindamage + "&&" + maxdamage + "&&" + movement + "&&" + fly + "&&" + ranged; //Give the string all the info we're sending
+        try { //We use try to make sure there's no crash in case of an error when sending the message
+            out.write(line + "\n"); //Add the string to the stream
+            out.flush(); //Sends the prepared message
         }catch (IOException e){
             System.err.println("Couldn't read or write");
             System.exit(1);
--- src/pokemonHeroes/NetworkRead.java
+++ src/pokemonHeroes/NetworkRead.java
@@ -1,10 +1,9 @@
 package pokemonHeroes;
 
-import java.awt.Color;
-import java.io.*;
-import java.net.*;
-
-import javax.swing.JOptionPane;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
 
 class NetworkRead implements Runnable{
     Socket socket;
@@ -18,19 +17,19 @@ class NetworkRead implements Runnable{
     public void run(){
         try{
 
-            BufferedReader in= new BufferedReader( new InputStreamReader(socket.getInputStream()));
-            String serverString;
+            BufferedReader in= new BufferedReader( new InputStreamReader(socket.getInputStream())); //What we use to get messages from the server
+            String serverString; //Create the string we'll be using
 
-            while ((serverString=in.readLine())!=null)
+            while ((serverString=in.readLine())!=null) //Stay in the while so long as we don't get a null message
             {
-                System.out.println("serverString=" + serverString);
+                System.out.println("serverString=" + serverString); //For debugging
 
-                if(serverString.startsWith("Bye")) {
+                if(serverString.startsWith("Bye")) { //If the message has bye it means we're quitting
 
-                    scene.endGame();
+                    scene.endGame(); //Go to end game function where the game is shut down
                     break;
                 }
-                else
+                else //If we're not quitting then send the message to scene where the variables we get will be used
                     scene.update(serverString);
             }
         }
