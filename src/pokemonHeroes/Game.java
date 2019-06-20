d78c57d4b2feeb61829c397d50504261b7e748cd
Hydrataur <rexzhydrataur@gmail.com>
2019-06-20 18:29:14 +0300

    Added default size for non main comp

--- src/pokemonHeroes/Game.java
+++ src/pokemonHeroes/Game.java
@@ -25,6 +25,7 @@ public class Game {
                 frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); //Terminates app when closing window so that it doesn't run in the background
                 frame.setExtendedState(JFrame.MAXIMIZED_BOTH); //Sets width and height of frame depending on screen size
                 //frame.setUndecorated(true);    //Add this later. Removes borders allowing fullscreen.
+                frame.setSize(1533, 875);
 
                 Scene scene = new Scene(frame); //Panel we draw on
 
